ba8f3c076c74314204fbf93c263736a5ef6ea334
RainingNight <leshin@live.com>
2019-01-06 12:54:08 +0800

    Add FetchData Page

---
+++ src/Web/Pages/FetchData.cshtml.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+
+namespace Web.Pages
+{
+    public class FetchDataModel : PageModel
+    {
+        private static HttpClient _client;
+
+        public FetchDataModel(IHttpClientFactory httpClientFactory)
+        {
+            _client = httpClientFactory.CreateClient("api");
+        }
+
+        public IList<WeatherForecast> Forecasts { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            var res = await _client.GetStringAsync("/api/SampleData/WeatherForecasts");
+            Forecasts = JsonConvert.DeserializeObject<IList<WeatherForecast>>(res);
+        }
+    }
+
+    public class WeatherForecast
+    {
+        public string DateFormatted { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF { get; set; }
+
+        public string Summary { get; set; }
+    }
+}
